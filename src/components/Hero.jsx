67bcbced21f29a2ed6c54074a6e9d9ae642931b4
Adwaith Rajeev <162904916+AdwaithRajeev@users.noreply.github.com>
2026-04-03 00:45:34 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -283,7 +283,7 @@ const Hero = () => {
             whileHover={{ scale: 1.05 }}
             whileTap={{ scale: 0.95 }}
           >
-            {'> Download Resume'}
+            {'Download Resume'}
           </motion.a>
 
           {/* Social Links */}
